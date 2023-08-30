1313549200320c6957dfd17d5a44be9e77ce87f2
ENR813 <50517527+ENR813@users.noreply.github.com>
2023-08-30 12:49:38 +0200

    Update Addon_better.py

---
+++ Addon_better.py
@@ -0,0 +1,10 @@
+bl_info = {
+    "name": "My Threaded BulletPhysics python Binds",
+    "category": "Object",
+}
+
+# Add complete script here in future Something similar to whats in BrokenExamleGuiCode.py
+def register():
+    print("Hello World")
+def unregister():
+    print("Goodbye World")
